9f0c2eef7a86fddc4990b744396c789b61ff452e
Hamish Knox <hrjkknox@gmail.com>
2021-03-25 15:11:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+#gluelib
+This is a simple library for interacting with the pastebin API. The code is fairly simple and self-documenting, and all other information needed is contained within this file.
+(It's called gluelib because pastelib was taken)
+
+##Terminology
+###Session Keys
+When using this library, the first thing to do is generate a session key. This is what you will use instead of a username and password. Call `getSessionKey` with your pastebin username, password, and developer key, and you will receive a session key. This is used in all future API calls. Keep it in memory, and generate a new one for every session. You do not need to use your username and password again.
+
+###Paste Keys
+These are the parts of a pastebin URL that identify a specific paste. (e.g. `zvwcdXyn` in `https://pastebin.com/zvwcdXyn`)
+
+##Functions
+
+###`getPaste(pasteKey, sessionKey, devKey)`
+Returns the text of a paste given the above parameters.
+
+###`makePaste(pasteName, pasteText, devKey, sessionKey, expiryDate = "N", privacy = "0")`
+Uploads a paste to pastebin with the above parameters. See [the pastebin API page](https://pastebin.com/doc_api "the pastebin API page") for more detail.
+
+###`deletePaste(pasteKey, sessionKey, devKey)`
+Deletes a paste given the above parameters.
+
+###`getSessionKey(username, password, devKey)`
+Returns a session key when given a pastebin username, password, and developer key. See the section on session keys for more.
+
+###`outputBytesToString(outputBytes)`
+This function formats a response from the pastebin API. It removes the prefix and suffix bytes, converts the response to a string, and returns it. All other functions in this library pass their output through it. You do not need to use this function in your code.
+
---
+++ src/gluelib/__init__.py
@@ -0,0 +1,59 @@
+import requests
+
+# Removes formatting and converts the API output to a string
+def outputBytesToString(outputBytes):
+	strVersion = str(outputBytes)
+	strVersion = strVersion[:-1]
+	strVersion = strVersion[2:]
+	strVersion = strVersion.replace("\\r\\n", "\n")
+	return strVersion
+
+def getSessionKey(username, password, devKey):
+	# Construct the query
+	paramaters = {
+		"api_dev_key": devKey,
+		"api_user_name": username,
+		"api_user_password": password
+		}
+	# Send request and get response
+	response = requests.post(url = "https://pastebin.com/api/api_login.php", data = paramaters)
+	# Remove formatting data
+	sessionKey = outputBytesToString(response.content)
+
+def getPaste(pasteKey, sessionKey, devKey):
+	paramaters = {
+		"api_dev_key": devKey,
+		"api_user_key": sessionKey,
+		"api_paste_key": pasteKey,
+		"api_option": "show_paste"
+		}
+	response = requests.post(url = "https://pastebin.com/api/api_raw.php", data = paramaters)
+	pasteText = outputBytesToString(response.content)
+	return pasteText
+
+# Posts a paste, and returns the paste ID.
+# Full details are available at https://pastebin.com/api
+def makePaste(pasteName, pasteText, devKey, sessionKey, expiryDate = "N", privacy = "0"):
+	paramaters = {
+		"api_option": "paste",
+		"api_dev_key": devKey,
+		"api_paste_code": pasteText,
+		"api_paste_private": privacy,
+		"api_paste_name": pasteName,
+		"api_paste_expire_date": expiryDate,
+		"api_user_key": sessionKey
+		}
+	response = requests.post(url = "https://pastebin.com/api/api_post.php", data = paramaters)
+	output = outputBytesToString(response.content)
+	return output
+
+def deletePaste(pasteKey, sessionKey, devKey):
+	paramaters = {
+		"api_dev_key": devKey,
+		"api_user_key": sessionKey,
+		"api_paste_key": pasteKey,
+		"api_option": "delete"
+		}
+	response = requests.post(url = "https://pastebin.com/api/api_post.php", data = paramaters)
+	output = outputBytesToString(response.content)
+	return output
